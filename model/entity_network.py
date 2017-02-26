ccbbdf70415b8e7b8ca75e9e1171561a1c06f180
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2017-02-25 22:43:28 -0500

    Add shift attention for stability

--- model/entity_network.py
+++ model/entity_network.py
@@ -5,15 +5,28 @@ Model definition class for the Recurrent Entity Network. Defines Input Encoder,
 Dynamic Memory Cell, and Readout components.
 """
 from collections import namedtuple
+from tflearn.activations import sigmoid, softmax
+import functools
 import tensorflow as tf
-import tensorflow.nn.rnn_cell_impl._RNNCell as RNNCell
-from tflearn.activations import sigmoid, softmax, prelu
 
 PAD_ID = 0
 
+def prelu_func(features, initializer=None, scope=None):
+    """
+    Implementation of [Parametric ReLU](https://arxiv.org/abs/1502.01852) borrowed from Keras.
+    """
+    with tf.variable_scope(scope, 'PReLU', initializer=initializer):
+        alpha = tf.get_variable('alpha', features.get_shape().as_list()[1:])
+        pos = tf.nn.relu(features)
+        neg = alpha * (features - tf.abs(features)) * 0.5
+        return pos + neg
+prelu = functools.partial(prelu_func, initializer=tf.constant_initializer(1.0))
+
+
 class EntityNetwork():
-    def __init__(self, vocabulary, sentence_len, story_len, batch_size, memory_slots=20, embedding_size=100, 
-                 learning_rate=0.01, initializer=tf.truncated_normal_initializer(stddev=0.1)):
+    def __init__(self, vocabulary, sentence_len, story_len, batch_size, memory_slots, embedding_size, 
+                 learning_rate, decay_steps, decay_rate, clip_gradients=40.0, 
+                 initializer=tf.random_normal_initializer(stddev=0.1)):
         """
         Initialize an Entity Network with the necessary hyperparameters.
 
@@ -21,15 +34,19 @@ class EntityNetwork():
         :param sentence_len: Maximum length of a sentence.
         :param story_len: Maximum length of a story.
         """
-        self.vocab_sz, self.sentence_len, self.story_len = len(vocabulary), max_sentence, story_len
+        self.vocab_sz, self.sentence_len, self.story_len = len(vocabulary), sentence_len, story_len
         self.embed_sz, self.memory_slots, self.init = embedding_size, memory_slots, initializer
-        self.bsz, self.learning_rate = batch_size
+        self.bsz, self.lr, self.decay_steps, self.decay_rate = batch_size, learning_rate, decay_steps, decay_rate
+        self.clip_gradients = clip_gradients
 
         # Setup Placeholders
         self.S = tf.placeholder(tf.int32, [None, self.story_len, self.sentence_len], name="Story")
         self.S_len = tf.placeholder(tf.int32, [None], name="Story_Length")
         self.Q = tf.placeholder(tf.int32, [None, self.sentence_len], name="Query")
-        self.A = tf.placeholder(tf.int32, [None], name="Answer")
+        self.A = tf.placeholder(tf.int64, [None], name="Answer")
+
+        # Setup Global Step 
+        self.global_step = tf.Variable(0, trainable=False)
 
         # Instantiate Network Weights
         self.instantiate_weights()
@@ -42,6 +59,10 @@ class EntityNetwork():
 
         # Build Training Operation
         self.train_op = self.train()
+
+        # Create operations for computing the accuracy
+        correct_prediction = tf.equal(tf.argmax(self.logits, 1), self.A)
+        self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     
     def instantiate_weights(self):
         """
@@ -49,14 +70,18 @@ class EntityNetwork():
         Memory Cell, as well as Output Decoder.
         """
         # Create Embedding Matrix, with 0 Vector for PAD_ID (0)
-        self.E = tf.get_variable("Embedding", [self.vocab_sz, self.embed_sz], initializer=self.init)
-        self.E *= tf.constant([0] + [1 for _ in range(self.vocab_sz - 1)], tf.float32, shape=[self.vocab_sz, 1])
+        E = tf.get_variable("Embedding", [self.vocab_sz, self.embed_sz], initializer=self.init)
+        zero_mask = tf.constant([0 if i == 0 else 1 for i in range(self.vocab_sz)], 
+                                    dtype=tf.float32, shape=[self.vocab_sz, 1])
+        self.E = E * zero_mask
         
         # Create Learnable Mask
-        self.input_mask = tf.get_variable("Input_Mask", [self.sentence_len, self.embed_sz], initializer=self.init)
+        self.story_mask = tf.get_variable("Story_Mask", [self.sentence_len, 1], initializer=tf.constant_initializer(1.0))
+        self.query_mask = tf.get_variable("Query_Mask", [self.sentence_len, 1], initializer=tf.constant_initializer(1.0))
 
         # Create Memory Cell Keys [IF DESIRED - TIE KEYS HERE]
-        self.keys = [tf.get_variable("Key_%d" % i, [self.embed_sz], initializer=self.init) for i in range(self.memory_slots)]
+        self.keys = [tf.get_variable("Key_%d" % i, [self.embed_sz], initializer=self.init) 
+                         for i in range(self.memory_slots)]
         
         # Create Memory Cell
         self.cell = DynamicMemory(self.memory_slots, self.embed_sz, self.keys)
@@ -72,54 +97,60 @@ class EntityNetwork():
         """
         # Story Input Encoder
         story_embeddings = tf.nn.embedding_lookup(self.E, self.S)             # Shape: [None, story_len, sent_len, embed_sz]
-        story_embeddings = tf.multiply(story_embeddings, self.input_mask)     # Shape: [None, story_len, sent_len, embed_sz]
+        story_embeddings = tf.multiply(story_embeddings, self.story_mask)     # Shape: [None, story_len, sent_len, embed_sz]
         story_embeddings = tf.reduce_sum(story_embeddings, axis=[2])          # Shape: [None, story_len, embed_sz]
 
         # Query Input Encoder
         query_embedding = tf.nn.embedding_lookup(self.E, self.Q)              # Shape: [None, sent_len, embed_sz]
-        query_embedding = tf.multipy(query_embedding, self.input_mask)        # Shape: [None, sent_len, embed_sz]
+        query_embedding = tf.multiply(query_embedding, self.query_mask)       # Shape: [None, sent_len, embed_sz]
         query_embedding = tf.reduce_sum(query_embedding, axis=[1])            # Shape: [None, embed_sz]
 
         # Send Story through Memory Cell
         initial_state = self.cell.zero_state(self.bsz, dtype=tf.float32)
-        _, memories = tf.nn.dynamic_rnn(self.cell, story_embeddings, self.S_len, initial_state)
+        _, memories = tf.nn.dynamic_rnn(self.cell, story_embeddings, sequence_length=self.S_len, 
+                                        initial_state=initial_state)
 
         # Output Module 
-        scores = []
-        for m in memories:
-            score = tf.reduce_sum(tf.multiply(query_embedding, m), axis=[-1]) # Shape: [None]
-            score = tf.expand_dims(score, axis=-1)                            # Shape: [None, 1]
-            scores.append(score)
+        stacked_memories = tf.stack(memories, axis=1)
         
         # Generate Memory Scores
-        p_scores = softmax(tf.concat(scores, axis=[1]))                       # Shape: [None, mem_slots]
+        p_scores = softmax(tf.reduce_sum(tf.multiply(stacked_memories,        # Shape: [None, mem_slots]
+                                                     tf.expand_dims(query_embedding, 1)), axis=[2]))
         
-        # Get Weighted Sum of Memories
-        attention = tf.expand_dims(p_scores, axis=-1)                         # Shape: [None, mem_slots, 1]
-        memory_stack = tf.stack(memories, axis=1)                             # Shape: [None, mem_slots, embed_sz]
-        u = tf.reduce_sum(attention * memory_stack, axis=1)                   # Shape: [None, embed_sz]
+        # Subtract max for numerical stability (softmax is shift invariant)
+        p_max = tf.reduce_max(p_scores, axis=-1, keep_dims=True)
+        attention = tf.nn.softmax(p_scores - p_max)       
+        attention = tf.expand_dims(attention, 2)                              # Shape: [None, mem_slots, 1]
+
+        # Weight memories by attention vectors
+        u = tf.reduce_sum(tf.multiply(stacked_memories, attention), axis=1)   # Shape: [None, embed_sz]
 
         # Output Transformations => Logits
         hidden = prelu(tf.matmul(u, self.H) + query_embedding)                # Shape: [None, embed_sz]
         logits = tf.matmul(hidden, self.R)                                    # Shape: [None, vocab_sz]
+        
         return logits
 
     def loss(self):
         """
         Build loss computation - softmax cross-entropy between logits, and correct answer. 
         """
-        return tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.logits, labels=self.A))
+        return tf.losses.sparse_softmax_cross_entropy(self.A, self.logits)
     
     def train(self):
         """
         Build ADAM Optimizer Training Operation.
-        """"
-        opt = tf.train.AdamOptimizer(self.learning_rate)
-        return opt.minimize(self.loss_val)
+        """
+        learning_rate = tf.train.exponential_decay(self.lr, self.global_step, self.decay_steps, 
+                                                   self.decay_rate, staircase=True)
+        train_op = tf.contrib.layers.optimize_loss(self.loss_val, global_step=self.global_step, 
+                                                   learning_rate=learning_rate, optimizer="Adam",
+                                                   clip_gradients=self.clip_gradients)
+        return train_op
 
-class DynamicMemory(RNNCell):
+class DynamicMemory(tf.contrib.rnn.RNNCell):
     def __init__(self, memory_slots, memory_size, keys, activation=prelu,
-                 initializer=tf.truncated_normal_initializer(stddev=0.1)):
+                 initializer=tf.random_normal_initializer(stddev=0.1)):
         """
         Instantiate a DynamicMemory Cell, with the given number of memory slots, and key vectors.
 
@@ -146,26 +177,32 @@ class DynamicMemory(RNNCell):
     @property
     def output_size(self):
         return [self.mem_sz for _ in range(self.m)]
+    
+    def zero_state(self, batch_size, dtype):
+        """
+        Initialize Memory to start as Key Values
+        """
+        return [tf.tile(tf.expand_dims(key, 0), [batch_size, 1]) for key in self.keys]
 
     def __call__(self, inputs, state, scope=None):
         """
         Run the Dynamic Memory Cell on the inputs, updating the memories with each new time step.
 
-        :param inputs: 3D Tensor of shape [bsz, story_len, mem_sz] representing story sentences.
-        :param states: List of length M, each with 2D Tensor [bsz, mem_sz] => h_j (starts as 0).
+        :param inputs: 2D Tensor of shape [bsz, mem_sz] representing a story sentence.
+        :param states: List of length M, each with 2D Tensor [bsz, mem_sz] => h_j (starts as key).
         """
         new_states = []
         for block_id, h in enumerate(state):
             # Gating Function
             content_g = tf.reduce_sum(tf.multiply(inputs, h), axis=[1])                  # Shape: [bsz]
             address_g = tf.reduce_sum(tf.multiply(inputs, 
-                                      tf.expand_dims(0, self.keys[block_id])), axis=[1]) # Shape: [bsz]
+                                      tf.expand_dims(self.keys[block_id], 0)), axis=[1]) # Shape: [bsz]
             g = sigmoid(content_g + address_g)
 
             # New State Candidate
             h_component = tf.matmul(h, self.U)                                           # Shape: [bsz, mem_sz]
-            w_component = tf.matmul(tf.expand_dims(0, self.keys[block_id]), self.V)      # Shape: [1, mem_sz]
-            s_component = tf.matmul(s, self.W)                                           # Shape: [bsz, mem_sz]
+            w_component = tf.matmul(tf.expand_dims(self.keys[block_id], 0), self.V)      # Shape: [1, mem_sz]
+            s_component = tf.matmul(inputs, self.W)                                      # Shape: [bsz, mem_sz]
             candidate = self.activation(h_component + w_component + s_component)         # Shape: [bsz, mem_sz]
 
             # State Update
@@ -175,4 +212,4 @@ class DynamicMemory(RNNCell):
             new_h_norm = tf.nn.l2_normalize(new_h, -1)                                   # Shape: [bsz, mem_sz]
             new_states.append(new_h_norm)
         
-        return new_states
+        return new_states, new_states
--- run.py
+++ run.py
@@ -3,12 +3,23 @@ run.py
 
 Core script for building, training, and evaluating a Recurrent Entity Network.
 """
+from model.entity_network import EntityNetwork
 from preprocessor.reader import parse
 import tensorflow as tf
 
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_integer("task_id", 1, "ID of Task to Train/Evaluate [1 - 20].")
 tf.app.flags.DEFINE_string("data_path", "tasks/en-valid-10k", "Path to Training Data")
+tf.app.flags.DEFINE_integer("embedding_size", 100, "Dimensionality of word embeddings.")
+tf.app.flags.DEFINE_integer("memory_slots", 20, "Number of dynamic memory slots.")
+tf.app.flags.DEFINE_integer("batch_size", 32, "Batch size for training/evaluating.")
+tf.app.flags.DEFINE_integer("num_epochs", 200, "Number of Training Epochs.")
+tf.app.flags.DEFINE_float("learning_rate", .01, "Learning rate for ADAM Optimizer.")
+tf.app.flags.DEFINE_integer("decay_epochs", 25, "Number of epochs to run before learning rate decay.")
+tf.app.flags.DEFINE_float("decay_rate", 0.5, "Rate of decay for learning rate.")
+tf.app.flags.DEFINE_float('clip_gradients', 40.0, 'Norm to clip gradients to.')
+
+
 
 def main(_):
     # Get Vectorized Forms of Stories, Questions, and Answers
@@ -21,7 +32,28 @@ def main(_):
     assert(trainQ.shape[1] == valQ.shape[1] == testQ.shape[1])
 
     # Build Model
-    entity_net = EntityNetwork(word2id)
+    with tf.Session() as sess:
+        # Instantiate Model
+        entity_net = EntityNetwork(word2id, trainS.shape[2], trainS.shape[1], FLAGS.batch_size,
+                                   FLAGS.memory_slots, FLAGS.embedding_size, FLAGS.learning_rate, 
+                                   FLAGS.decay_epochs * (trainS.shape[0]/FLAGS.batch_size), FLAGS.decay_rate)
+        
+        # Initialize all Variables
+        sess.run(tf.global_variables_initializer())
+
+        # Start Training Loop
+        n, bsz = trainS.shape[0], FLAGS.batch_size
+        for epoch in range(FLAGS.num_epochs):
+            loss, acc, counter = 0.0, 0.0, 0
+            for start, end in zip(range(0, n, bsz), range(bsz, n, bsz)):
+                curr_loss, curr_acc, _ = sess.run([entity_net.loss_val, entity_net.accuracy, entity_net.train_op], 
+                                                  feed_dict={entity_net.S: trainS[start:end], 
+                                                             entity_net.S_len: trainS_len[start:end],
+                                                             entity_net.Q: trainQ[start:end],
+                                                             entity_net.A: trainA[start:end]})
+                loss, acc, counter = loss + curr_loss, acc + curr_acc, counter + 1
+                if counter % 100 == 0:
+                    print "Epoch %d Batch %d Average Loss:" % (epoch, counter), loss / float(counter), "Average Accuracy:", acc / float(counter)
 
 if __name__ == "__main__":
     tf.app.run()
